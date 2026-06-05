e8419fc45c3821e93f3bd6bdb9e605370cefd85a
Vincent Jaquet-Hertgen <vincent.hertgen@gmail.com>
2026-06-05 10:09:04 +0200

    Modification ConfigReader.java

--- ConfigReader.java
+++ ConfigReader.java
@@ -1,39 +1,49 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-
-import static java.lang.Integer.parseInt;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
 public class ConfigReader {
-    public SiteConfig lireConfig(File f){
+
+    static public SiteConfig lireConfig(String path) {
         SiteConfig site;
-        int p = 0;
+        int p =0;
         String fd = "";
         String cl = "";
         String cle = "";
         try {
-            BufferedReader bf = new BufferedReader(new FileReader(f));
-            String ligne = bf.readLine();
-            while (ligne != null) {
-                String mot ="";
-                int i =1;
-                while(ligne.charAt(i)!='>') {
-                    mot += ligne.charAt(i);
-                    i++;
-                }
-                switch (mot){
-                    case "port" : p = parseInt(ligne.substring(7, 11));
-                    case "DocumentRoot" : fd = ligne.substring(15, 23);
-                    case "Acceslog" : cl = ligne.substring(11, 25);
-                    case "Errorlog" : cle = ligne.substring(11, 24);
-                }
-                ligne = bf.readLine();
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document doc = builder.parse(path);
+            doc.getDocumentElement().normalize();
+
+            // Port
+            NodeList portList = doc.getElementsByTagName("port");
+            if (portList.getLength() > 0) {
+                p = (Integer.parseInt(portList.item(0).getTextContent().trim()));
+            }
+
+            // DefaultIndex
+            NodeList indexList = doc.getElementsByTagName("DefaultIndex");
+            if (indexList.getLength() > 0) {
+                fd = (indexList.item(0).getTextContent().trim());
+            }
+            // AccesLog
+            NodeList accesLogList = doc.getElementsByTagName("Acceslog");
+            if (accesLogList.getLength() > 0) {
+                cl = (accesLogList.item(0).getTextContent().trim());
+            }
+            // ErrorLog
+            NodeList errorLogList = doc.getElementsByTagName("Errorlog");
+            if (errorLogList.getLength() > 0) {
+                cle = (errorLogList.item(0).getTextContent().trim());
             }
             site = new SiteConfig(p, fd, cl, cle);
 
         } catch (Exception e) {
             site = null;
         }
+
+
         return site;
     }
 }
